298735fef4dc6fcb8e8987fda8d3de92cfc6c32b
Tomaso Leoni <tomasoleoni011@gmail.com>
2020-04-23 15:24:41 +0200

    elaborate file analyze method on backend

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/testGet", (req, res, next) => {
+router.post("/textUpload", (req, res, next) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
 
-});
+    } if (req.body.agb != null) {
+        try {
+            var hints = ['schaden', 'verbindlich', 'kosten', 'pflicht', 'haftung'];
+            var agb = req.body.agb.split(/[?.!]/);
+            console.log(agb);
+            var dangerousLines = [];
 
-router.post("/testPost", (req, res, next) => {
+            agb.forEach(line => {
+                hints.forEach(hint => {
+                    if (line.toLowerCase().includes(hint)) {
+                        dangerousLines.push({
+                            "line": line,
+                            "danger": hint
+                        });
+                    }
+                })
+            });
+
+            res.status(200).send(dangerousLines);
+        } catch (error) {
+            res.status(400).send("An error occurred while analyzing your text.");
+        }
+    } else {
+        res.status(400).send("No content or file was sent.");
+    }
 
 });
 
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
+import AddIcon from "@material-ui/icons/Add";
+import { useDropzone } from 'react-dropzone'
 import "./home.css"
 
 
 export default function Home() {
+    const [textFile, setTextFile] = useState(0);
+    const onDrop = useCallback(acceptedFiles => {
+        if (acceptedFiles) {
+            setTextFile(acceptedFiles);
+        }
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
     return (
         <div className="container" id="container">
             <div className="row justify-content-center text-center h-100 m-0 p-0">
                 <div className="col-10 my-auto">
                     <h1 className="mb-0 mt-n3 font-weight-bold" >AGB Analyzer</h1>
-                    <small class="text-muted"><em>by Tomaso Leoni</em></small>
+                    <small className="text-muted"><em>by Tomaso Leoni</em></small>
                 </div>
                 <div className="col-6">
                     <TextField id="outlined-basic" label="Paste the Terms and Conditions here ..." variant="outlined" fullWidth />
+                    <div
+                        className="grabbing w-25 mt-5 py-3 px-2 border rounded dropzone-border text-center d-inline-block "
+                        {...getRootProps()}
+                    >
+                        <input {...getInputProps()} />
+                        <AddIcon color="disabled" fontSize="large"/>
+                        {isDragActive ? (
+                            <p className="text-secondary my-auto">Drop it here ...</p>
+                        ) : textFile ? <p className="text-secondary my-auto">{textFile.name}</p> : <p className="text-secondary my-auto">Drop your text file here</p>}
+                    </div>
                 </div>
             </div>
         </div>
